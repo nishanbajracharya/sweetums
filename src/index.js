e7842be64f6d0c5a23995891ad60b4383975e4b8
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-02 20:54:30 +0545

    Add express and basic routing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const dotenv = require('dotenv');
+const express = require('express');
+
+const routes = require('./routes');
+const constants = require('./constants');
 
 dotenv.config();
+
+const port = process.env.PORT || constants.PORT;
+
+const app = express();
+
+app.use('/slack', routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/submit', (req, res) => {
+  res.send('');
+});
+
+module.exports = router;
